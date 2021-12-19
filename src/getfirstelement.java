5afd7b74cc16bc68af3d925cda2455894af1819f
jailsonveiga <jmdaveiga2012@gmail.com>
2021-12-19 17:58:04 -0500

    Return The First Element In An Array

---
+++ src/getfirstelement.java
@@ -0,0 +1,14 @@
+// Return The First Element In An Array
+
+public class getfirstelement {
+
+    public static void main(String[] args) {
+
+        System.out.println(getfirstindex(new int[] {3, 1, 4}));
+    }
+
+    public static int getfirstindex(int[] arr) {
+
+        return arr[0];
+    }
+}
