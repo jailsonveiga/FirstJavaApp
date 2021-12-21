e75e3005ae25ce2dd69b28befcc0c1349ad18ed1
jailsonveiga <jmdaveiga2012@gmail.com>
2021-12-20 23:02:39 -0500

    Return the Next Number from the Integer Passed

---
+++ src/nextnumber.java
@@ -0,0 +1,12 @@
+//Return the Next Number from the Integer Passed
+
+public class nextnumber {
+    public static void main(String[] args) {
+        System.out.println("Return the Number after 6? " +  addition(6));
+        System.out.println("Return the Number after 43? " +  addition(43));
+        System.out.println("Return the Number after 0? " +  addition(0));
+    }
+    public static int addition(int num) {
+        return num + 1;
+    }
+}
