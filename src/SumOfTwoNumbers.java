6d584de472fb81f72dc8b6322c0a587ae02d978b
jailsonveiga <jmdaveiga2012@gmail.com>
2021-12-23 21:40:42 -0500

    Return the Sum of Two Numbers

---
+++ src/SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+// Return the Sum of Two Numbers
+
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        System.out.println("return the sum of two numbers: (5, 11) equal " + sum(5,11));
+
+        System.out.println("return the sum of two numbers: (15, 11) equal " + sum(15,11));
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
